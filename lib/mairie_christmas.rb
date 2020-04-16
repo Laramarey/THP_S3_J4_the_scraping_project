705484660dda1601a498d4d82644f5e09d3f96ac
Laramarey <l.schutz@tbs-education.org>
2020-04-16 20:19:35 +0200

    Détails et rspec

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 
 #Ne pas oublier de bien mettre les guillemets
 
-URL = "http://annuaire-des-mairies.com/"
+URL = "https://annuaire-des-mairies.com/val-d-oise.html"
 
 #Isoler les élements que je souhaite avoir dans mon tableau
 
@@ -17,63 +17,73 @@ URL = "http://annuaire-des-mairies.com/"
 #   { "ville_2" => "email_2" }, 
 #   etc
 # ]
-# je distingue donc deux méthodes à faire, une pour les maries et une pour les e,mails
+# je distingue donc deux méthodes minimum à faire, une pour les noms des maries et une pour les emails
 
 #regardons comment se construit l'url http://annuaire-des-mairies.com/95/ableiges.html
 
-#95 / number département
-#def number end
 
-#url_val_oise = "http://annuaire-des-mairies.com/95/"
-x = []
+#------------------------------------------------------------------------------------
+#liste des urls de mairie dans le 95 : programme pour sortir toutes les urls sans avoir à cliquer
 
-def townhall
 
-#xpath = "//*[@id="voyance-par-telephone"]/table/tbody/tr[2]/td/table/tbody/tr/td[1]/p/a[1]
-page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/"))
-
-townhall = page.xpath('//*[@id="voyance-par-telephone"]/table/tbody/tr[2]/td/table/tbody/tr/td[1]/p/a[1]').each do |node|
-puts node.text
-
-return townhall	
+def townhall_url_95
+specific_townhall_url = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com/val-d-oise.html")).xpath('//a[@class="lientxt"]/@href')  
+    #chaque mairie a une url qui suit une certaine construction, on peut ajouter le xpath qui inclu le lien href
+  townhall_url = specific_townhall_url.map {|x| "https://www.annuaire-des-mairies.com" + x.text[1..-1]}
+  #à l'url de ma ville est donc égale au schéma d'url de ville surlequel je réalise une opération (map) sans remplacer le précédent tableau
+  #et dans cette opération à chaque nouvel élément j'ajoute mon texte du href
+  return townhall_url
 end
 
+#------------------------------------------------------------------------------------
 
-def townhall_url
-puts "http://annuaire-des-mairies.com/95/"+townhall.downcase+ ".html" << x
-puts x.compact.join
-return x.compact.join = townhall_url
-end
+#Liste des emails des mairies : scraping des emails de maries avec fonction scraping et recherche
 
 
+#1/Liste des villes--------------------scraping des villes
+
+def townhall_95
+  page = Nokogiri::HTML(open("https://annuaire-des-mairies.com/val-d-oise.html"))
+    townhall_95 = page.xpath('//a[@class="lientxt"]') # scrape moi toutes les villes dans la page val d'oise
+  return townhall_95
+  end
+# puts townhall_95
 
 def get_townhall_email(townhall_url)
-#xpathemail = "/html/body/div/main/section[2]/div/table/tbody/tr[4]/td"
-page = Nokogiri::HTML(open(townhall_url))
 
-get_townhall_email = page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |node|
-puts node.text
+#je mets une condition si l'URL est nil ou empty
+  return nil if townhall_url.nil? || townhall_url.empty?
 
-return get_townhall_email
+#2/Liste des emails--------------------je fais une recherche intelligente de type 'contient'
+
+  list_mail = townhall_url_95.map{ |x| #pour avoir ma liste des emails je vais effectuer une opération sur mon tableau des url de chaque mairie
+      Nokogiri::HTML(open(x)).xpath('//*[contains(text(), "@")]') #ainsi pour tout élément x de mon tableau, donc chaque url de mairie, scrape le texte qui entoure un "@"
+        }
+#array des mails
+   
+  mail_hash = []# je créé mon arr
+  for a in 0..townhall_95.length-1 do 
+    mail_hash[a] = Hash.new
+    mail_hash[a][townhall_95[a].text] = list_mail[a].text
+  end
+  puts mail_hash
+  return mail_hash
 end
 
-def mairie_xmas
+#-------------------------Si mon hash est vide !--------------------------------------------------
 
-result = []
+def mail_hash
 
-for a in 0..townhall.size do result[a] = Hash.new
-	result[a][townhall[a].text] = get_townhall_email[a].text
+  if mail_hash.empty? #si un hash du tableau est vide, alors ne me mets pas d'erreur nil mais juste "pas de mail"
+  return "Pas de mail"
+  end
 end
 
-puts result
-retunr result
+#---------------------------------ma méthode de fin pour vérifier qu'il n'y ait pas de mic mac avec mes variables-----------------------------------------------
 
+def testing
+  townhall_url = townhall_url_95
+  get_townhall_email(townhall_url) 
 end
-
-
-
-
-
-
-
+testing
 
---
+++ spec/mairie_christmas_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/mairie_christmas'
+
+
+describe "get_townhall_email method, it takes an url and gives back a hash" do
+  it "test no url" do
+    expect(get_townhall_email("")).to eq(nil)
+  end
+  it "test good url" do
+    expect(get_townhall_email("http://annuaire-des-mairies.com/95/nerville-la-foret.html")).to eq({"nerville-la-foret"=>"mairie-nerville95@wanadoo.fr"})
+  end
+  it "test good url" do
+    expect(get_townhall_email("http://annuaire-des-mairies.com/95/nerville-la-foret.html")).to be_kind_of(Hash)
+  end
+  
+end
